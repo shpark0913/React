9e175375a40d899e5858a64f9cf497e0bc8ab734
shpark0913 <shpark097754@gmail.com>
2023-01-12 14:20:42 +0900

    :sparkles: Study state
    - useState를 통해 mode 변경

--- React개정판_생활코딩/230111/src/App.js
+++ React개정판_생활코딩/230111/src/App.js
@@ -1,25 +1,36 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 ////////////////////////////////////////////////////////////////////////////
 function Header(props){
   return  <header>
-  <h1><a href='/'>{ props.title }</a>
+  <h1><a href='/' onClick={(event) => {
+    event.preventDefault()
+    props.onChangeMode()
+  }}>{ props.title }</a>
   </h1>
 </header>
 }
+
 ////////////////////////////////////////////////////////////////////////////
 function Nav(props){
   const lis = []
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];    
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{ t.title }</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={(event) => {
+        event.preventDefault()
+        props.onChangeMode(Number(event.target.id))
+    }}>{ t.title }</a></li>)
   }
-  return <nav>
+  return <nav> 
   <ol>
     { lis }
   </ol>
 </nav>
 }
+
 ////////////////////////////////////////////////////////////////////////////
 function Article(props){
   return <article>
@@ -27,19 +38,43 @@ function Article(props){
   { props.body }
 </article>
 }
+
 ////////////////////////////////////////////////////////////////////////////
 
 function App() {
-  const topics = [
+  // const _mode = useState('WELCOME')
+  // const mode = _mode[0]
+  // const setMode = _mode[1]
+  const [mode, setMode] = useState('WELCOME')
+  const [id, setId] = useState(null)
+  const topics = [ 
     {id: 1, title: 'html', body: 'html is ...'},
     {id: 2, title: 'css', body: 'css is ...'},
     {id: 3, title: 'javascript', body: 'javascript is ...'},
   ]
+  let content = null
+  if (mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB!"></Article>
+  } else if (mode === 'READ'){
+    let title, body = null
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title
+        body = topics[i].body
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
   return (
     <div className="App">
-      <Header title="REACT"></Header>
-      <Nav topics={topics}></Nav>
-      <Article title="Welcome" body="Hello, WEB!"></Article>
+      <Header title="REACT" onChangeMode={() => {
+        setMode('WELCOME')
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(_id) => {
+        setMode('READ')
+        setId(_id)
+      }}></Nav>
+      { content }
     </div>
   );
 }
