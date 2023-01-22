cbe5f79969a3dbc7d31d2f599b2c0f3825f5cbd8
shpark0913 <shpark097754@gmail.com>
2023-01-22 23:40:03 +0900

    :art: Update 기능 복습
    - 복습을 하며 학습한 사항들
      - tag의 속성으로 숫자를 입력하면 문자가 됨.
        - 이를 해결하기 위해 Number 사용.
      - event.target은 event를 유발시킨 tag를 가리킴.
      - onSubmit은 submit을 클릭했을 때 form tag에서 발생하는 이벤트
      - form tag는 submit을 하면 페이지가 리로드됨.

--- React개정판_생활코딩/src/App.js
+++ React개정판_생활코딩/src/App.js
@@ -1,143 +1,134 @@
-import { editableInputTypes } from '@testing-library/user-event/dist/utils';
 import './App.css';
-import { useState } from 'react';
+import {useState} from 'react';
 
 function Header(props) {
   return <header>
-  <h1><a href="/" onClick={(event) => {
-    event.preventDefault()
-    props.onChangeMode()
-  }}>{props.title}</a></h1>
-</header>
+    <h1><a href="/" onClick={event => {
+      event.preventDefault()
+      props.onChangeMode()
+    }}>{props.title}</a></h1>
+  </header>
 }
 
 
 function Nav(props) {
   const lis = []
   for (let i = 0; i < props.topics.length; i++) {
-    let element = props.topics[i];
-    lis.push(<li key={element.id}>
-      <a id={element.id} href={"/read/" + element.id} onClick={(event) => {
-      event.preventDefault()
-      // props.onChangeMode(element.id)
-
-      // element.id는 숫자였지만 태그의 속성으로 넘기면 걔는 문자가 된다!!!
-      props.onChangeMode(Number(event.target.id))
-      // target은 이벤트를 유발시키는 태그를 가리킴!! => 여기서는 a Tag
-      // a Tag의 id를 가져오고 싶으니 event.target.id !!
-
-    }}>{element.title}</a>
-    </li>)
+    lis.push(<li key={props.topics[i].id}>
+        {/* tag의 속성으로 숫자를 입력하면 문자가 됨 */}
+        <a id={props.topics[i].id} href={"/read/" + props.topics[i].id}
+          onClick={event => {
+            event.preventDefault()
+            // event.target은 event를 유발시킨 tag를 가리킨다
+            props.onChangeMode(Number(event.target.id))
+        }}>
+          {props.topics[i].title}
+        </a>
+      </li>)
   }
   return <nav>
-  <ol>
-    {lis}
-  </ol>
-</nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
 }
 
 
 function Article(props) {
   return <article>
-  <h2>{props.title}</h2>
-  {props.body}
-</article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
 }
 
 
 function Create(props) {
   return <article>
     <h2>Create</h2>
-
-    {/* onSubmit은 submit버튼을 클릭했을 때 form 태그에서 발생하는 event  */}
-    {/* event.target 은 event가 발생한 tag를 가리킴 */}
+    {/* onSubmit 은 submit을 클릭했을 때 form 태그에서 발생하는 이벤트 */}
+    {/* form 태그는 submit을 하면 페이지가 리로드됨 */}
     <form onSubmit={event => {
       event.preventDefault()
+      // event.target은 form 태그.
       const title = event.target.title.value
       const body = event.target.body.value
       props.onCreate(title, body)
     }}>
-      <p><input type="text" name="title" placeholder='title'></input></p>
-      <p><textarea name="body" placeholder="body"></textarea></p>
-      <input type="submit" value="Create"></input>
+      <p><input type="text" name="title" placeholder='title'/></p>
+      <p><textarea name="body" placeholder='body'></textarea></p>
+      <p><input type="submit" value="Create" /></p>
     </form>
   </article>
 }
+
 
 function Update(props) {
   const [title, setTitle] = useState(props.title)
   const [body, setBody] = useState(props.body)
   return <article>
-    <h2>Update</h2>
-    <form onSubmit={event => {
-      event.preventDefault()
-      const title = event.target.title.value
-      const body = event.target.body.value
-      props.onUpdate(title, body)
-    }}>
-      <p><input type="text" name="title" value={title} onChange={event=>{
-        setTitle(event.target.value)
-      }}></input></p>
-      <p><textarea name="body" value={body} onChange={event=>{
-        setBody(event.target.value)
-      }}></textarea></p>
-      <input type="submit" value="Update"></input>
-    </form>
-  </article>
+  <h2>Update</h2>
+  <form onSubmit={event => {
+    event.preventDefault()
+    const title = event.target.title.value
+    const body = event.target.body.value
+    props.onUpdate(title, body)
+  }}>
+    <p><input type="text" name="title" placeholder='title' value={title} onChange={event => {
+      setTitle(event.target.value)
+    }}></input></p>
+    <p><textarea name="body" placeholder='body' value={body} onChange={event => {
+      setBody(event.target.value)
+    }}></textarea></p>
+    <p><input type="submit" value="Update" /></p>
+  </form>
+</article>
 }
 
 
 function App() {
 
-  // 0번째 원소는 상태의 값을 읽을 때 쓰는 데이터
-  // 1번째 원소는 그 상태를 변경할 때 사용하는 함수
-  // useState의 인자는 그 state의 초기값
-  // state의 값은 0번째 인덱스, 바꿀 때는 1번째 인덱스의 함수로 바꿈
-  // const _mode = useState('WELCOME')
-  // const mode = _mode[0]
-  // const setMode = _mode[1]
   const [mode, setMode] = useState('WELCOME')
-
   const [id, setId] = useState(null)
   const [nextId, setNextId] = useState(4)
+
   const [topics, setTopics] = useState([
-    {id: 1, title: 'html', body: 'html is ...'},
-    {id: 2, title: 'css', body: 'css is ...'},
-    {id: 3, title: 'javascript', body: 'javascript is ...'},
+    {id: 1, title: "html", body: "html is ..."},
+    {id: 2, title: "css", body: "css is ..."},
+    {id: 3, title: "JavaScript", body: "JavaScript is ..."}
   ])
 
   let content = null
   let contextControl = null
 
-  if(mode === 'WELCOME'){
+  if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB!"></Article>
-  } else if(mode === 'READ'){
+  } else if (mode === 'READ') {
     let title, body = null
     for (let i = 0; i < topics.length; i++) {
-      if (id === topics[i].id) {
+      if (topics[i].id === id){
         title = topics[i].title
         body = topics[i].body
       }
     }
     content = <Article title={title} body={body}></Article>
-    contextControl = <li><a href={"/update/" + id} onClick={(event) => {
+    contextControl = <li><a href={"/update/" + id} onClick={event=>{
       event.preventDefault()
-      setMode('UPDATE')
-    }}>Update</a></li>
-  } else if(mode === 'CREATE'){
-    content = <Create onCreate={(_title, _body) => {
-      const newTopic = {id: nextId, title: _title, body: _body}
+      setMode("UPDATE")
+    }}>UPDATE</a></li>
+  } else if (mode === 'CREATE') {
+    content = <Create onCreate={(title, body) => {
+      const newTopic = {id: nextId, title: title, body: body}
       const newTopics = [...topics]
-      newTopics.push(newTopic) 
+      newTopics.push(newTopic)
       setTopics(newTopics)
       setMode('READ')
       setId(nextId)
       setNextId(nextId + 1)
     }}></Create>
-  } else if(mode === 'UPDATE'){
+  } else if (mode === 'UPDATE') {
     let title, body = null
     for (let i = 0; i < topics.length; i++) {
-      if (id === topics[i].id) {
+      if (topics[i].id === id){
         title = topics[i].title
         body = topics[i].body
       }
@@ -145,38 +136,41 @@ function App() {
     content = <Update title={title} body={body} onUpdate={(title, body) => {
       const updatedTopic = {id: id, title: title, body: body}
       const newTopics = [...topics]
-      for (let i=0; i < newTopics.length; i++) {
+      for(let i=0; i<newTopics.length; i++) {
         if(newTopics[i].id === id){
           newTopics[i] = updatedTopic
-          break
         }
-        setTopics(newTopics)
-        setMode('READ')
       }
+      setTopics(newTopics)
+      setMode("READ")
     }}></Update>
   }
 
-
   return (
     <div>
-      <Header title="WEB" onChangeMode={() => {
-        setMode('WELCOME')
-      }}></Header>
+    <Header title="WEB" onChangeMode={() => {
+      setMode('WELCOME')
+    }}></Header>
 
-      <Nav topics={topics} onChangeMode={(_id) => {
-        setMode('READ')
-        setId(_id)
-      }}></Nav>
-      
-      {content} <br />
-      <ul>
-        <li><a href="/create" onClick={event => {
+
+    <Nav topics={topics} onChangeMode={(id)=>{
+      setMode('READ')
+      setId(id)
+    }}></Nav>
+
+
+    { content }
+    <ul>
+      <li>
+        <a href="/create" onClick={event => {
           event.preventDefault()
           setMode('CREATE')
-        }}>Create</a></li>
+        }}>CREATE</a>
+      </li>
 
-        {contextControl}
-      </ul>
+      {contextControl}
+
+    </ul>
     </div>
   );
 }
