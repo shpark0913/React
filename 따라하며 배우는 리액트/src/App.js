d4d13550e619bc9efc7f44d0473c4001ed9b1f71
shpark0913 <shpark097754@gmail.com>
2023-01-29 23:29:26 +0900

    :sparkles: React 공부
    - Redux
    - Redux Hooks
    - 로그인 페이지

---
+++ 따라하며 배우는 리액트/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage"
+// import NavBar from "./components/views/NavBar/NavBar"
+// import Footer from "./components/views/Footer/Footer"
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />}></Route>
+          <Route exact path="/login" element={<LoginPage />}></Route>
+          <Route exact path="/register" element={<RegisterPage />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ 따라하며 배우는 리액트/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import { useEffect } from 'react'
+// import axios from 'axios'
+
+function LandingPage() {
+
+  // // setupProxy.js 참고
+  // useEffect(() => {
+  //   axios.get('/api/hello')
+  //   .then((response) => console.log(response.data))
+  // }, [])
+
+
+  return (
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      width: '100%', height: '100vh'
+    }}>
+      <h2>시작 페이지</h2>
+    </div>
+  )
+}
+
+export default LandingPage
