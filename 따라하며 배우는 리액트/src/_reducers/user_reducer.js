0d6c7a20a295f86ca1253bef2e88a824e6e0d3ce
shpark0913 <shpark097754@gmail.com>
2023-01-30 17:53:43 +0900

    :sparkles: 회원가입, 로그인 기능 완성, 다양한 컴포넌트 생성
    - 회원가입, 로그인 FE 코드 완성
    - Navbar, Footer 컴포넌트 생성

--- 따라하며 배우는 리액트/src/_reducers/user_reducer.js
+++ 따라하며 배우는 리액트/src/_reducers/user_reducer.js
@@ -1,16 +1,27 @@
 import {
-    LOGIN_USER, REGISTER_USER
+    LOGIN_USER, REGISTER_USER, CHECK_EMAIL, CHECK_NICKNAME
 } from '../_actions/types'
 
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = {}, action) {
     switch (action.type) {
         case LOGIN_USER:
             return { ...state, loginSuccess: action.payload }
-            break
+            break;
+
         case REGISTER_USER:
             return { ...state, register: action.payload }
-            break
+            break;
+
+        case CHECK_EMAIL:
+            return { ...state, emailValid: action.payload}
+            break;
+
+        case CHECK_NICKNAME:
+            return { ...state, nicknameVaild: action.payload}
+            break;
+            
         default:
             return state
     }
--- 따라하며 배우는 리액트/src/components/views/NavBar/NavBar.js
+++ 따라하며 배우는 리액트/src/components/views/NavBar/NavBar.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import { useNavigate, NavLink } from 'react-router-dom'
+import logo from '../../../assets/글씨X_130.png'
 
 function NavBar() {
+  const navigate = useNavigate()
   return (
-    <div>
-      NavBar
-    </div>
+    <nav>
+
+
+      <div>
+        <img src={logo} alt="" onClick={event => {
+          event.preventDefault()
+          navigate("/")
+        }}/>
+      </div>
+
+      <div>
+        <NavLink to="/arts">
+            작품</NavLink>
+      </div>
+
+      <div>
+        <NavLink to="/curations">큐레이션</NavLink>
+      </div>
+
+      <div>
+        <NavLink to="/commissions">커미션</NavLink>
+      </div>
+
+      <div>
+        <label htmlFor="search"></label>
+        <input type="text" name='search' placeholder='search'/>
+      </div>
+
+      <div>
+        <NavLink to="/login">로그인</NavLink>
+      </div>
+
+      <div>
+        <NavLink to="/register">회원가입</NavLink>
+      </div>
+      
+    </nav>
   )
 }
 
